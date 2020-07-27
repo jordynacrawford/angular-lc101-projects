188799533a921cb5e069b4c5cde27edf263f328a
Jordyn Crawford <jordynacrawford@gmail.com>
2020-07-27 16:44:29 -0500

    part 1 complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Super Dope Pics';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://www.netclipart.com/pp/m/56-560795_arrow-arrows-archery-navigation-comments-adventure-arrows-clip.png';
+  image3 = 'https://us.123rf.com/450wm/hchjjl/hchjjl1503/hchjjl150302470/37605078-stock-vector-doodle-archery.jpg?ver=6';
 
   constructor() { }
 
